646c87b80d3b19f946736d3538e0d71849414dd2
Charles Delannoy <charles.delannoy@hei.fr>
2020-09-16 16:09:17 +0200

    Updates

---
+++ precache-manifest.d8a816ea108824058b7af74778759e2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5884c50d11f465e231924761c4db7acb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b97966eb657523117da",
+    "url": "/static/js/2.504ff18d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.504ff18d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc006ff607dbf3827e77",
+    "url": "/static/js/main.904370f9.chunk.js"
+  },
+  {
+    "revision": "3934f9aee1bd9f70cb02",
+    "url": "/static/js/runtime-main.4d3c81f5.js"
+  }
+]);
